f517afa8b4abd6aa0e9375dc0b248070f9beeb8c
yasu <rock.fish.junkie@gmail.com>
2021-08-15 10:26:38 +0900

    make grid-components

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -3,17 +3,11 @@
     @include('_posts-header')
 
     <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
+        
         @if ($posts->count())
             <x-post-featured-card :post="$posts[0]" />
-
-            @if ($posts->count()>1)
-                <div class="lg:grid lg:grid-cols-6">
-                    @foreach ($posts->skip(1) as $post)
-                        @dd($loop)
-                        <x-post-card :post="$post" class="col-span-2"/>
-                    @endforeach    
-                </div>
-            @endif
+            <x-post-grid :posts="$posts" />
+           
         @else
             <p class="text-center">
                 No posts yet. Please check back later.
