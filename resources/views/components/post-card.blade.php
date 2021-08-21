e4ed77bdeef6ed93c591aaac96098de48d2d177e
yasu <rock.fish.junkie@gmail.com>
2021-08-21 13:13:41 +0900

    category button components

--- resources/views/components/post-card.blade.php
+++ resources/views/components/post-card.blade.php
@@ -10,10 +10,7 @@
                     <div class="mt-8 flex flex-col justify-between">
                         <header>
                             <div class="space-x-2">
-                                
-                                <a href="{{ $post->category->slug }}"
-                                   class="px-3 py-1 border border-red-300 rounded-full text-red-300 text-xs uppercase font-semibold"
-                                   style="font-size: 10px">{{ $post->created_at->diffForHumans() }}</a>
+                                <x-category-button :category="$post->category" />
                             </div>
 
                             <div class="mt-4">
