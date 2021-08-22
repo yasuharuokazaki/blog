024c1ef9516535ed47ef754b87d4c8d764d78c00
yasu <rock.fish.junkie@gmail.com>
2021-08-22 20:54:59 +0900

    a small  js drop detour

--- resources/views/_posts-header.blade.php
+++ resources/views/_posts-header.blade.php
@@ -15,13 +15,28 @@
         <!--  Category -->
         <div class="relative flex lg:inline-flex items-center bg-gray-100 rounded-xl">
            
-           <div x-data="{show:false}">
-                <button @click="show = !show" class="block">Categories</button>
+           <div x-data="{show:false}" >
+                <button @click="show = !show" class="inline-flex py-2 pl-3 pr-9 text-sm font-semibold w-32" >
+                   
+                    Categories
 
-                <div x-show="show">
-                    <a href="#" class="block">One</a>
-                    <a href="#" class="block">Two</a>
-                    <a href="#" class="block">Three</a>
+                    <svg class="transform -rotate-90 absolute pointer-events-none" style="right: 12px;" width="22"
+                    height="22" viewBox="0 0 22 22">
+                        <g fill="none" fill-rule="evenodd">
+                            <path stroke="#000" stroke-opacity=".012" stroke-width=".5" d="M21 1v20.16H.84V1z">
+                            </path>
+                            <path fill="#222"
+                                    d="M13.854 7.224l-3.847 3.856 3.847 3.856-1.184 1.184-5.04-5.04 5.04-5.04z"></path>
+                        </g>
+                    </svg>
+
+                </button>
+
+                <div x-show="show" class="py-2 absolute bg-gray-100 mt-2 rounded-xl w-full z-50" style="display:none">
+                    <a href="#" class="block text-left px-3 text-xs leading-5 hover:bg-blue-500 focus:bg-blue-500 hover:text-white forcus:text-white">One</a>
+                    <a href="#" class="block text-left px-3 text-xs leading-5 hover:bg-blue-500 focus:bg-blue-500 hover:text-white forcus:text-white">Two</a>
+                    <a href="#" class="block text-left px-3 text-xs leading-5 hover:bg-blue-500 focus:bg-blue-500 hover:text-white forcus:text-white">Three</a>
+                    <a href="#" class="block text-left px-3 text-xs leading-5 hover:bg-blue-500 focus:bg-blue-500 hover:text-white forcus:text-white">a long href link</a>
                 </div>
            </div>
 
